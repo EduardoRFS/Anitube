0f8d542c4a3f700fb48b6ad95da4e9f9e7048848
eduardo <theeduardorfs@gmail.com>
2016-05-08 03:13:26 +0000

    New

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+'use strict';
 const express = require('express');
-const cheerio = $ = require('cheerio');
+const cheerio = require('cheerio');
+const $ = require('cheerio');
 const request =  require('request-promise').defaults({ 'proxy': 'http://107.191.61.141:3128' });
 const url = require('url');
 const path = require('path');
@@ -36,14 +38,13 @@ async function getPage (originalUrl, res) {
     let href = $(link).attr('href');
     if (!href) return;
     href = url.parse(href);
-    href.host = 'educhan.net.br';
+    href.host = 'localhost:3000';
     $(link).attr('href', url.format(href));
   });
   body('script').map((i, link) => {
     let src = $(link).attr('src');
     if (!src) return;
     if (src.indexOf('adingo.jp') > -1 || src.indexOf("anigrupo") > -1) {
-      console.log(src);
       $(link).remove();
       return;
     }
@@ -55,25 +56,43 @@ async function getPage (originalUrl, res) {
     if (!src.host) return;
 
     if (src.host.startsWith('www.anitube'))
-      src.host = 'educhan.net.br';
+      src.host = 'localhost:3000';
     $(link).attr('src', url.format(src));
   });
   return body.html();
 }
 
+app.get('/player/config.php', wrap(async (req, res) => {
+  let originalUrl = req.originalUrl;
+  let body = await getPage(originalUrl, res);
+  let index = body.indexOf('sources: [{');
+  let str = body.substr(index+9);
+  index = str.indexOf('}],');
+  let lines = str.substr(0, index+2).split('\n');
+  lines.forEach(line => {
+    if (line.indexOf('file: "') > -1) {
+      let urlo;
+      let url = urlo = line.split('"')[1];
+      url = url.replace('http://vid', 'http://lax');
+      url = url.replace('.cdn.', '.vid.');
+      body = body.replace(urlo, url);
+    }
+  });
+  res.send(body);
+}));
 app.use(wrap(async (req, res) => {
   let originalUrl = req.originalUrl;
   if (!cache[originalUrl]) {
-    cache[originalUrl] = await getPage(originalUrl);
+    cache[originalUrl] = await getPage(originalUrl, res);
   }
   res.send(cache[originalUrl]);
-  let body = await request.get({
+  /*let body = await request.get({
     url: ANITUBE + req.originalUrl,
     headers: {
       'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.7 Safari/537.36'
     },
     transform: autoParse
-  });
+  });*/
   //let body = await request(url);
   //console.log(body);
 }));
@@ -87,4 +106,4 @@ const r = request.defaults({ 'proxy': 'http://107.191.61.141:3128' });
   });
 })();*/
 
-app.listen(3000);
+app.listen(8081);
